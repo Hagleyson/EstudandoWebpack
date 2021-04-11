5d53f86fe79be64fb4c2c6a66ded82f543777fb3
hagleyson <hagleyson@gmail.com>
2021-04-11 02:50:58 -0300

    aplicando css utilizando o Webpack

--- src/principal.js
+++ src/principal.js
@@ -1,5 +1,6 @@
 //const Pessoa = require("./pessoa");
 import Pessoa from "./pessoa";
 import "./modulos/moduloA";
+import "./assets/css/style.css";
 const atendente = new Pessoa();
 console.log(atendente.cumprimentar());
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,21 @@ const webpack = require("webpack");
 module.exports = {
   mode: "development", //modo de desenvovimento
   entry: "./src/principal.js", //ponto de entrada
-  output: /*saida, vai salvar um arquivo um principal dentro da pasta public*/ {
+  //configurando pastas de Saida - nesse caso vai salvar um arquivo um principal dentro da pasta public
+  output: {
     filename: "principal.js",
     path: __dirname + "/public",
+  },
+  module: {
+    //rules -> array de loaders
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", //adiciona CSS a DOM injetando a tag <style>
+          "css-loader", //interpreta @import, url()....
+        ],
+      },
+    ],
   },
 };
